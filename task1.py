1a271dbd5a23f4cd2d675ac9e07c765349aba4d3
19  5  2  1  19  20  9  1  14 <sebastianm880@deltalearns.ca>
2022-01-19 11:08:14 -1100

    Update task1.py

--- task1.py
+++ task1.py
@@ -19,7 +19,7 @@ Enter number:4
 """
 import math 
 num2 = (1,2,3,4,5,6,7,8,9,10,11,12)
-num = input("enter a number")
+num = input("enter an integer")
 num = int(num)
 for x in num2:
     print(x * num)
